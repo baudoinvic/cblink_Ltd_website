c4388a94d95973c8bd4d4cdd09c12aafc83b90e5
mayelemas <mayelemas120@example.com>
2023-08-07 13:42:31 -0700

    made some changes to Demovideo

--- src/Components/DemoVideoComponent.tsx
+++ src/Components/DemoVideoComponent.tsx
@@ -10,11 +10,11 @@ const DemoVideoComponent = (props: Props) => {
     <div className='w-full  flex flex-col items-center justify-center'>
       <video className='my-8 rounded w-7/12' src={props.videoUrl} controls={true}></video>
       <div className='w-7/12 flex items-center justify-around'>
-        <Link target={'_blank'} to={'/auth/signup'}><button className='w-32 hover:bg-blue-700 h-12 mx-4 cursor-pointer text-lg rounded bg-cblink text-white flex items-center justify-center'>Get It</button></Link>
+     
+       <a href="auth/signup" className='w-32 hover:bg-blue-700 h-12 mx-4 cursor-pointer text-lg rounded bg-cblink text-white flex items-center justify-center'>Get It</a>
         <Link to={'/#meloproducts'}><button className=' hover:bg-blue-700 h-12 mx-4 cursor-pointer text-lg rounded bg-cblink text-white flex items-center justify-center'>Try Another Product</button></Link>
       </div>
     </div>
   )
 }
-
 export default DemoVideoComponent
